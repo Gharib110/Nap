94baad284c6a15bea618631bd984a86fd7441651
AlirezaGharib <alirezagharib110@gmail.com>
2022-01-20 01:31:35 +0330

    Project was Completed

--- auth.go
+++ auth.go
@@ -1,14 +1,59 @@
-package Nap
+package nap
 
-type Authentication interface {
-	AuthenticationHeader() string // basic <base64-encoded string>
-}
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
 
+// AuthToken for token authentication
 type AuthToken struct {
-	Token string `json:"token"`
+	Token string
 }
 
+// AuthBasic for basic authentication
 type AuthBasic struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string
+	Password string
+}
+
+// Authentication interface
+type Authentication interface {
+	AuthorizationHeader() string // "basic <base64-encoded string>"
+}
+
+// NewAuthToken creates a new auth token struct
+func NewAuthToken(token string) *AuthToken {
+	return &AuthToken{
+		Token: token,
+	}
+}
+
+// AuthorizationHeader returns the token header value
+func (a *AuthToken) AuthorizationHeader() string {
+	return fmt.Sprintf("token %s", a.Token)
+}
+
+// NewAuthBasic creates a new auth basic struct
+func NewAuthBasic(username, password string) *AuthBasic {
+	return &AuthBasic{
+		Username: username,
+		Password: password,
+	}
+}
+
+// AuthorizationHeader returns the basic auth header value
+func (a *AuthBasic) AuthorizationHeader() string {
+	buffer := &bytes.Buffer{}
+	enc := base64.NewEncoder(base64.URLEncoding, buffer)
+	encContent := fmt.Sprintf("%s:%s", a.Username, a.Password)
+	enc.Write([]byte(encContent))
+	enc.Close()
+	content, err := ioutil.ReadAll(buffer)
+	if err != nil {
+		log.Fatalln("Read failed:", err)
+	}
+	return fmt.Sprintf("basic %s", string(content))
 }
--- resource.go
+++ resource.go
@@ -1,7 +1,39 @@
-package Nap
+package nap
+
+import (
+	"bytes"
+	"html/template"
+	"io/ioutil"
+	"log"
+)
 
 type RestResource struct {
-	EndPoint string
-	Method   string
+	Endpoint string // /get/{{.user}} user=matt
+	Method   string // GET
 	Router   *CBRouter
+}
+
+func NewResource(endpoint, method string, router *CBRouter) *RestResource {
+	return &RestResource{
+		Endpoint: endpoint,
+		Method:   method,
+		Router:   router,
+	}
+}
+
+func (r *RestResource) RenderEndpoint(params map[string]string) string {
+	if params == nil {
+		return r.Endpoint
+	}
+	t, err := template.New("resource").Parse(r.Endpoint)
+	if err != nil {
+		log.Fatalln("Unable to parse endpoint")
+	}
+	buffer := &bytes.Buffer{}
+	t.Execute(buffer, params)
+	endpoint, err := ioutil.ReadAll(buffer)
+	if err != nil {
+		log.Fatalln("Unable to read endpoint")
+	}
+	return string(endpoint)
 }
